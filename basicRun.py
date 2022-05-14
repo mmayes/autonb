c46aa83da9f1083cb168705026bf995ae01035f0
mmayes <github@mhmaviation.com>
2022-05-14 11:21:01 -0700

    Cleared out a lot of commented out code from previous versions

--- basicRun.py
+++ basicRun.py
@@ -29,7 +29,6 @@ def basicRun(prefix, suffix,\
 
   groupDropdown = Select(browser.find_element_by_xpath(
     "//*[contains(@id,'add_run_to_queue_rungroups')]"))
-  # group = base + "-EMB-" + seat
   group = base + "-XMJ-" + seat
   if verbose:
     print("selecting " + group)
@@ -72,7 +71,9 @@ def basicRun(prefix, suffix,\
     print("min credit window: " + "floor-" + str(min_floor) + ", ceiling-" + str(min_ceiling) + ", threshold-" + str(min_threshold_hour) + ":" + str(min_threshold_minute))
     print("max credit window: " + "floor-" + str(max_floor) + ", ceiling-" + str(max_ceiling) + ", threshold-" + str(max_threshold_hour) + ":" + str(max_threshold_minute))
 
-  #normal credit window
+  #####################################
+  ## Normal credit window
+  #####################################
   a[5].send_keys(normal_floor)
   a[5].send_keys(Keys.TAB)
   a[6].send_keys(Keys.TAB)
@@ -84,7 +85,9 @@ def basicRun(prefix, suffix,\
   a[10].send_keys(normal_threshold_minute)
   a[10].send_keys(Keys.TAB)
 
-  #min credit window
+  #####################################
+  ## Min credit window
+  #####################################
   a[11].send_keys(min_floor)
   a[11].send_keys(Keys.TAB)
   a[12].send_keys(Keys.TAB)
@@ -96,7 +99,9 @@ def basicRun(prefix, suffix,\
   a[16].send_keys(min_threshold_minute)
   a[16].send_keys(Keys.TAB)
 
-  #max credit window
+  #####################################
+  ## Max credit window
+  #####################################
   a[17].send_keys(max_floor)
   a[17].send_keys(Keys.TAB)
   a[18].send_keys(Keys.TAB)
@@ -109,147 +114,6 @@ def basicRun(prefix, suffix,\
   a[22].send_keys(Keys.TAB)
 
   time.sleep(timeBetween)
-
-
-  # #####################################
-  # ## Max credit window
-  # #####################################
-  # if verbose:
-  #   print("max credit window: " + "floor-" + str(max_floor) + ", ceiling-" + str(max_ceiling) + ", threshold-" + str(max_threshold_hour) + ":" + str(max_threshold_minute))
-  #
-  #
-  # a[17].send_keys(Keys.BACKSPACE)
-  # a[17].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[17].send_keys(max_floor)
-  # time.sleep(timeBetween)
-  # a[17].send_keys(Keys.TAB)
-  # time.sleep(5)
-  #
-  # a[19].send_keys(Keys.BACKSPACE)
-  # a[19].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[19].send_keys(max_ceiling)
-  # time.sleep(timeBetween)
-  #
-  # a[21].send_keys(Keys.BACKSPACE)
-  # a[21].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # print("sending normal threshold hour of " + str(normal_threshold_hour))
-  # a[21].send_keys(max_threshold_hour)
-  # time.sleep(timeBetween)
-  # a[21].send_keys(Keys.TAB)
-  # a[22].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[22].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # if (max_threshold_minute > 40):
-  #   a[22].send_keys("45")
-  # elif (max_threshold_minute > 25):
-  #   a[22].send_keys("30")
-  # elif (max_threshold_minute > 10):
-  #   a[22].send_keys("15")
-  # else:
-  #   a[22].send_keys("00")
-  # time.sleep(timeBetween)
-  # #a[22].send_keys(Keys.TAB)
-  #
-  # #####################################
-  # ## Normal credit window
-  # #####################################
-  # if verbose:
-  #   print("normal credit window: " + "floor-" + str(normal_floor) + ", ceiling-" + str(normal_ceiling) + ", threshold-" + str(normal_threshold_hour) + ":" + str(normal_threshold_minute))
-  # a[5].send_keys(Keys.BACKSPACE)
-  # a[5].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[5].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[5].send_keys(normal_floor)
-  # time.sleep(timeBetween)
-  #
-  # a[7].send_keys(Keys.BACKSPACE)
-  # a[7].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[7].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[7].send_keys(normal_ceiling)
-  # time.sleep(timeBetween)
-  #
-  # time.sleep(.5)
-  # a[9].send_keys(Keys.BACKSPACE)
-  # a[9].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[9].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # print("sending normal threshold hour of " + str(normal_threshold_hour))
-  # a[9].send_keys(normal_threshold_hour)
-  # time.sleep(1)
-  # a[9].send_keys(Keys.TAB)
-  # a[10].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[10].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # if (normal_threshold_minute > 40):
-  #   a[10].send_keys("45")
-  # elif (normal_threshold_minute > 25):
-  #   a[10].send_keys("30")
-  # elif (normal_threshold_minute > 10):
-  #   a[10].send_keys("15")
-  # else:
-  #   a[10].send_keys("00")
-  # time.sleep(timeBetween)
-  # a[10].send_keys(Keys.TAB)
-  #
-  # #####################################
-  # ## Min credit window
-  # #####################################
-  # if verbose:
-  #   print("min credit window: " + "floor-" + str(min_floor) + ", ceiling-" + str(min_ceiling) + ", threshold-" + str(min_threshold_hour) + ":" + str(min_threshold_minute))
-  # a[11].send_keys(Keys.BACKSPACE)
-  # a[11].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[11].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[11].send_keys(min_floor)
-  # time.sleep(timeBetween)
-  #
-  # a[13].send_keys(Keys.BACKSPACE)
-  # a[13].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[13].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[13].send_keys(min_ceiling)
-  # time.sleep(timeBetween)
-  #
-  # time.sleep(.5)
-  # a[15].send_keys(Keys.BACKSPACE)
-  # a[15].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[15].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # print("sending normal threshold hour of " + str(normal_threshold_hour))
-  # a[15].send_keys(min_threshold_hour)
-  # time.sleep(1)
-  # a[15].send_keys(Keys.TAB)
-  # a[16].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[16].send_keys(Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # if (min_threshold_minute > 40):
-  #   a[16].send_keys("45")
-  # elif (min_threshold_minute > 25):
-  #   a[16].send_keys("30")
-  # elif (min_threshold_minute > 10):
-  #   a[16].send_keys("15")
-  # else:
-  #   a[16].send_keys("00")
-  # time.sleep(timeBetween)
-  # a[16].send_keys(Keys.TAB)
-
-  # a[23].send_keys(Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE)
-  # time.sleep(timeBetween)
-  # a[23].send_keys("50")
-  # time.sleep(timeBetween)
 
   ####################################
   ## Cancel or save
